a73db0b404aa31b8c4fca5a4b990b08e0e0d0a96
xSenny <ursuarsenie@gmail.com>
2024-07-16 13:35:24 +0300

    Finished all the checkout features

--- app/(admin)/admin/orders/page.tsx
+++ app/(admin)/admin/orders/page.tsx
@@ -1,6 +1,25 @@
-const OrdersPage = () => {
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { ProductsDataTable } from "@/components/products-table/data-table";
+import { columns } from "@/components/orders-table/columns";
+const OrdersPage = async () => {
+
+  const {orders} = await getAllOrders()
+
   return (
-    <div></div>
+    <div className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-foreground'>
+      <Card className='max-w-[93vw]'>
+        <CardHeader>
+          <CardTitle>Orders</CardTitle>
+          <CardDescription>Manage your customer's orders</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ProductsDataTable columns={columns} data={orders!} />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,23 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
+
+export const getTimePassedSince = (date: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - date.getTime();
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return days === 1 ? '1 day ago' : `${days} days ago`;
+  } else if (hours > 0) {
+    return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
+  } else if (minutes > 0) {
+    return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
+  } else {
+    return 'Just now';
+  }
+}
